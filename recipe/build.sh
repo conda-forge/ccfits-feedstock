adfcc7f902a8fede3927996f4416fa44eeeb5674
Matthew R Becker <becker.mr@gmail.com>
2018-12-13 08:00:22 -0600

    this should work

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,18 +8,19 @@ cmake \
     -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX} \
     -DBUILD_SHARED_LIBS=on ..
 
-# make CCfits
-#
-# # really cmake?
-# # cookbook looks for installed headers which are not there
-# # because you cannot install without building the cookbook which
-# # won't build due to the lack of installed headers
-# # hence I am hacking them in by hand
-# mkdir -p ${PREFIX}/include/CCfits
-# cp ../*.h ${PREFIX}/include/CCfits/.
-# make cookbook
+make CCfits
 
-make
+# really cmake?
+# cookbook looks for installed headers which are not there
+# because you cannot install without building the cookbook which
+# won't build due to the lack of installed headers
+# hence I am hacking them in by hand
+# it appears cmake uses a glob as well for the headers so nothing
+# extra is leaking into the build
+mkdir -p ${PREFIX}/include/CCfits
+cp ../*.h ${PREFIX}/include/CCfits/.
+make cookbook
+
 make install
 
 # Remove the cookbook binary
